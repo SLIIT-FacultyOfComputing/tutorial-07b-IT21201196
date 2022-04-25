475a195e8a0e8e87fe89d2a6a2542f9e3d12a7b8
IT21201196 <99582425+IT21201196@users.noreply.github.com>
2022-04-25 16:49:09 +0000

    tute 7

--- Exercise02.cpp
+++ Exercise02.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main() {
+  
   // ========= DO NOT CHANGE THE INPUT CODE BELOW ==================
    int length, width, radius;
    cout << "Enter length of Rectangle : ";
@@ -12,6 +13,7 @@ int main() {
    cin >> width;
    cout << "Enter radius of Circle : ";
    cin >> radius;
+    
    // ======== DO NOT CHANGE THE CODE GIVEN ABOVE ====================
    
    // 1. Create a dynamic Rectangle type variable (pointer)
@@ -22,6 +24,24 @@ int main() {
    // 6. Call the display method of the Circle Object
    // 7. Delete the Rectangle Object from memory
    // 8. Delete the Circle Object from memory
+  Rectangle *rec1;
+  Rectangle *rec2;
+  rec2=new Rectangle(length,width);
+  Circle *c1;
+  Circle *c2;
+  c2=new Circle(radius);
+
+  rec1->display();
+  rec2->display();
+
+  c1->display();
+  c2->display();
+  delete rec1;
+  delete  rec2;
+  delete c1;
+  delete c2;
+
+  
   
   
    // ============ DO NOT CHANGE THE CODE BELOW ===================
